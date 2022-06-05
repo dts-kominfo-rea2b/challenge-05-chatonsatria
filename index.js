29f6d1b1acd2cb16024b55a691a808c89596c7ca
chatonsatria <martin1998.mcsy@gmail.com>
2022-06-05 14:17:49 +0700

    [Challenge Callback] - Done

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = (names, sortMethod) => {
+  let sortedName = sortMethod(names)
+  // cek log nama yang di passing
+  console.log("Hasil Sorting Ke Sort Function", sortedName)
+  // variable baru simpan nama dengan nomor
+  let sortedNamesWithNumber = []
+  let number = 1;
+  sortedName.forEach(data => {
+    sortedNamesWithNumber.push(`${number++}. ` + data)
+  });
+  return sortedNamesWithNumber
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  console.log("Ascending",names.sort())
+  return names.sort()
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  console.log("Descending", names.sort().reverse())
+  return names.sort().reverse()
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
